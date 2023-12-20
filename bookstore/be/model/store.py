048d7822a4096b3730d02b8822f7f61cf8a5a058
NOExsisted <QQMMRT163@163.com>
2023-12-20 14:49:30 +0800

    create tables

--- bookstore/be/model/store.py
+++ bookstore/be/model/store.py
@@ -1,63 +1,94 @@
 import logging
-import os
-import sqlite3 as sqlite
+import pymysql
 
 
 class Store:
     database: str
 
-    def __init__(self, db_path):
-        self.database = os.path.join(db_path, "be.db")
+    def __init__(self):
+        self.database = pymysql.connect(
+            host = '127.0.0.1',  # 数据库主机名
+            port = 3306,               # 数据库端口号，默认为3306
+            user = 'root',             # 数据库用户名
+            password='root',
+            db = 'bookstore',               # 数据库名称
+            charset = 'utf8'           # 字符编码
+        )
         self.init_tables()
 
     def init_tables(self):
         try:
             conn = self.get_db_conn()
-            conn.execute(
+            cursor = conn.cursor()
+            cursor.execute(
                 "CREATE TABLE IF NOT EXISTS user ("
-                "user_id TEXT PRIMARY KEY, password TEXT NOT NULL, "
-                "balance INTEGER NOT NULL, token TEXT, terminal TEXT);"
+                "user_id VARCHAR(255) PRIMARY KEY, password VARCHAR(255) NOT NULL, "
+                "balance INTEGER NOT NULL, token TEXT, terminal VARCHAR(255));"
             )
 
-            conn.execute(
+            cursor.execute(
                 "CREATE TABLE IF NOT EXISTS user_store("
-                "user_id TEXT, store_id, PRIMARY KEY(user_id, store_id));"
+                "user_id VARCHAR(255), store_id VARCHAR(255), PRIMARY KEY(user_id, store_id));"
             )
 
-            conn.execute(
+            cursor.execute(
                 "CREATE TABLE IF NOT EXISTS store( "
-                "store_id TEXT, book_id TEXT, book_info TEXT, stock_level INTEGER,"
-                " PRIMARY KEY(store_id, book_id))"
+                "store_id VARCHAR(255), book_id VARCHAR(255), book_info MEDIUMTEXT, stock_level INTEGER,"
+                " PRIMARY KEY(store_id, book_id));"
             )
 
-            conn.execute(
+            cursor.execute(
+                "CREATE TABLE IF NOT EXISTS book("
+                "store_id VARCHAR(255), "
+                "id VARCHAR(255) PRIMARY KEY, title VARCHAR(255), author VARCHAR(255), "
+                "publisher VARCHAR(255), original_title VARCHAR(255), "
+                "translator VARCHAR(255), pub_year VARCHAR(255),"
+                "pages INTEGER, price INTEGER, currency_unit VARCHAR(255),"
+                "binding VARCHAR(255), isbn VARCHAR(255),"
+                "author_intro TEXT, book_intro TEXT, content TEXT,"
+                "tags VARCHAR(255), picture MEDIUMBLOB);"
+                #"stock_level INTEGER);"
+            )
+
+            cursor.execute(
                 "CREATE TABLE IF NOT EXISTS new_order( "
-                "order_id TEXT PRIMARY KEY, user_id TEXT, store_id TEXT)"
+                "order_id VARCHAR(255) PRIMARY KEY, store_id VARCHAR(255), "
+                "user_id VARCHAR(255), book_status INTEGER, order_time DATETIME);"
             )
 
-            conn.execute(
+            cursor.execute(
                 "CREATE TABLE IF NOT EXISTS new_order_detail( "
-                "order_id TEXT, book_id TEXT, count INTEGER, price INTEGER,  "
-                "PRIMARY KEY(order_id, book_id))"
+                "user_id VARCHAR(255), order_id VARCHAR(255), book_id VARCHAR(255), "
+                "count INTEGER, price INTEGER, flag INTEGER, "
+                "PRIMARY KEY(order_id, book_id));"
+            )
+
+            cursor.execute(
+                "CREATE TABLE IF NOT EXISTS new_order_paid( "
+                "order_id VARCHAR(255), user_id VARCHAR(255), store_id VARCHAR(255), "
+                "book_status INTEGER, price INTEGER, "
+                "PRIMARY KEY(order_id, user_id));"
             )
 
             conn.commit()
-        except sqlite.Error as e:
+        except pymysql.Error as e:
             logging.error(e)
             conn.rollback()
 
-    def get_db_conn(self) -> sqlite.Connection:
-        return sqlite.connect(self.database)
+    def get_db_conn(self) -> pymysql.Connection:
+        return self.database
 
 
-database_instance: Store = None
+database_instance = Store()
 
 
-def init_database(db_path):
+def init_database():
     global database_instance
-    database_instance = Store(db_path)
+    database_instance = Store()
 
 
 def get_db_conn():
     global database_instance
     return database_instance.get_db_conn()
+
+init_database()
